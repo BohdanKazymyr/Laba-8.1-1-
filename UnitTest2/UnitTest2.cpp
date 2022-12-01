d3eba560cb80e15357488884cae30343e1d9e915
krabster <bohdan.kzymyr.ik.2022@lpnu.ua>
2022-12-01 16:12:11 +0200

    f

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laba 8(it)/Laba 8(it).cpp"
+#pragma warning(disable : 4996)
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			char str[101]{};
+			Count(str);
+			char* dest = new char[151];
+			Change(str);
+		}
+	};
+}
